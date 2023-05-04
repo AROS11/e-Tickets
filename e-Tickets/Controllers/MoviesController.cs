ab0db4fc36fba5d6b165b6c9d741b628a0ec5cd4
gentianaymeri1 <gentianaymeri1@gmail.com>
2023-05-04 02:26:24 +0200

    Adding the cinemas view

--- e-Tickets/Controllers/MoviesController.cs
+++ e-Tickets/Controllers/MoviesController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using e_Tickets.Data;
+using eTickets.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace e_Tickets.Controllers
 {
     public class MoviesController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
         {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProducert = await _context.Movies.ToListAsync();
             return View();
         }
     }
