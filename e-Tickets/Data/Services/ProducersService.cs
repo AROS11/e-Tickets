ca7c5948c7fe1eecfe8ea6ef3ffdb2ddaea15892
gentianaymeri1 <gentianaymeri1@gmail.com>
2023-06-07 16:01:38 +0200

    ProducersService

---
+++ e-Tickets/Data/Services/ProducersService.cs
@@ -0,0 +1,18 @@
+﻿using e_Tickets.Data;
+using e_Tickets.Models;
+using eTickets.Data.Base;
+using eTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+    public class ProducersService : EntityBaseRepository<Producer>, IProducersService
+    {
+        public ProducersService(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
